51df0846d30cea623a3702be222f26342cb785e7
krasimiranova <krasy_nova@yahoo.com>
2023-12-11 07:12:57 +0100

    auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { TourModule } from './tour/tour.module';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
